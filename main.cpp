49a37a88f10fa8f5c44ba4bda0dfe47c712fa3a5
vyalich <dimvyalkov97@gmail.com>
2020-09-16 01:51:52 +0300

    test commit

--- main.cpp
+++ main.cpp
@@ -4,10 +4,10 @@ void displayMe(void)
 {
     glClear(GL_COLOR_BUFFER_BIT);
     glBegin(GL_POLYGON);
-        glVertex3f(0.5, 0.0, 0.5);
-        glVertex3f(0.5, 0.0, 0.0);
-        glVertex3f(0.0, 0.5, 0.0);
-        glVertex3f(0.0, 0.0, 0.5);
+        glVertex3f(0.2, 0.0, 0.2);
+        glVertex3f(0.9, 0.0, 0.0);
+        glVertex3f(0.0, 0.9, 0.0);
+        glVertex3f(0.0, 0.0, 0.9);
     glEnd();
     glFlush();
 }
